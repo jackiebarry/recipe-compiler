bc6e72c5610f5a7f1827426e6aa6eb7e834af252
jackiebarry <54484334+jackiebarry@users.noreply.github.com>
2022-03-09 19:46:07 -0400

    created submit function

---
+++ Index.js
@@ -0,0 +1,189 @@
+let ingredients = [
+    "Chicken",
+    "Salmon",
+    "Ground Beef",
+    "Steak",
+    "Pork Tenderloin",
+    "Sausages",
+    "Lamb",
+    "Bacon",
+    "Chicken Stock",
+    "Vegetable Stock",
+    "Snap Peas",
+    "Celery",
+    "Cherry Tomatoes",
+    "Spinach",
+    "Red Onion",
+    "Mushroom",
+    "Radish",
+    "Shallots",
+    "Leek",
+    "Sweet Potatoes",
+    "Jalapeno",
+    "Green Peas",
+    "Garlic",
+    "Cucumber",
+    "Zucchini",
+    "Cauliflower",
+    "Carrot",
+    "Broccoli",
+    "Bell Pepper",
+    "Avocado",
+    "Eggplant",
+    "Potatoes",
+    "Eggs",
+    "Bread",
+    "Naan Bread",
+    "Pesto",
+    "Diced Tomatoes",
+    "Spaghetti",
+    "Penne",
+    "Tomato Paste",
+    "Rice",
+    "Black Beans",
+    "Chickpeas",
+    "Kidney Beans",
+    ];
+    
+    createIngredientForm(ingredients);
+    function createIngredientForm(ingredients) {
+    let form = document.getElementById("ingredientSelections"); 
+
+    for ( let i = 0; i < ingredients.length; i++) {
+        let input = document.createElement("input");
+        input.setAttribute("type", "checkbox");
+        input.value = ingredients[i];
+        input.id ='checkbox-' + ingredients[i];
+        let label = document.createElement('label');
+        label.textContent = ingredients[i];
+        label.setAttribute('for', input.id);
+        form.appendChild(input);
+        form.appendChild(label);
+    }
+    let submit = document.createElement('input');
+    submit.setAttribute("type", "submit");
+    submit.setAttribute("value", "submit");
+    form.appendChild(submit);
+    };
+
+// let ingredients = [
+//     {
+//         name: 'Chicken',
+        
+//     },
+//     {
+//         name: 'Salmon',
+    
+//     },
+//     {
+//         name: 'Ground Beef',
+//     },
+//     {
+//         name: 'Pork Tenderloin',
+//     },
+//     {
+//         name: 'Steak',
+//     },
+//     {
+//         name: 'Sausages',
+//     },
+//     {
+//         name: 'Lamb',
+//     },
+//     {
+//         name: 'Chicken Stock',
+//     },
+//     {
+//         name: 'Green Peas',
+//     },
+//     {
+//         name: 'Diced Tomatoes',
+        
+//     },
+//     {
+//         name: 'Sweet Potatoes',
+    
+//     },
+//     {
+//         name: 'Potatoes',
+//     },
+//     {
+//         name: 'Tomatoes',
+//     },
+//     {
+//         name: 'Spinach',
+//     },
+//     {
+//         name: 'Red Onions',
+//     },
+//     {
+//         name: 'Avocado',
+//     },
+//     {
+//         name: 'Cucumbers',
+//     },
+//     {
+//         name: 'Pasta',
+//     }
+//     ];
+
+// const info = document.querySelector(#info);
+// let details = ingredients.map(function(choice){
+//     return '<div class="choice">' + choice.name + '</div>'
+// });
+
+// info.innerHTML = ingredients.join('\n');
+
+// const ingredientOptions = [
+// "Chicken",
+// "Salmon",
+// "Ground Beef",
+// "Steak",
+// "Pork Tenderloin",
+// "Sausages",
+// "Lamb",
+// "Bacon",
+// "Chicken Stock",
+// "Vegetable Stock",
+// "Snap Peas",
+// "Celery",
+// "Cherry Tomatoes",
+// "Spinach",
+// "Red Onion",
+// "Mushroom",
+// "Radish",
+// "Shallots",
+// "Leek",
+// "Sweet Potatoes",
+// "Jalapeno",
+// "Green Peas",
+// "Garlic",
+// "Cucumber",
+// "Zucchini",
+// "Cauliflower",
+// "Carrot",
+// "Broccoli",
+// "Bell Pepper",
+// "Avocado",
+// "Eggplant",
+// "Potatoes",
+// "Eggs",
+// "Bread",
+// "Naan Bread",
+// "Pesto",
+// "Diced Tomatoes",
+// "Spaghetti",
+// "Penne",
+// "Tomato Paste",
+// "Rice",
+// "Black Beans",
+// "Chickpeas",
+// "Kidney Beans",
+// ];
+
+// const ingredients = document.querySelector('#ingredientOptions');
+// let ingredientChoices = ingredients.map(function(item) {
+//     return '<div class ="item">' + item + '</div>'
+// });
+
+// ingredients.innerHTML = ingredientChoices.join('\n');
