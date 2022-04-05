5ad6845f588152bbaca7e2587e1294cb99b1236d
jackiebarry <54484334+jackiebarry@users.noreply.github.com>
2022-04-05 14:58:23 -0300

    randomized recipe is possible

--- Index.js
+++ Index.js
@@ -84,48 +84,73 @@ async function sendApiRequest(array) {
     let APP_ID = 'adeda542'
     let API_KEY = '92f5597b299396e6134f2ef3942df3d4' 
     let choices = array.toString()
-    let response = await fetch(`https://api.edamam.com/search?app_id=${APP_ID}&app_key=${API_KEY}&q=${choices}`);
+    let response = await fetch(`https://api.edamam.com/search?app_id=${APP_ID}&app_key=${API_KEY}&q=${choices}&"mealType=lunch,dinner"&"imageSize=thumbnail"`);
     console.log(response)
     let data = await response.json()
     console.log(data) 
+    populateCarousel()
     useApiData(data)
 };
 
 function useApiData(data){
-document.getElementById("img1").src=data.hits[0].recipe.image 
-document.getElementById("img2").src=data.hits[1].recipe.image
-document.getElementById("img3").src=data.hits[2].recipe.image
+const hits = data.hits;
+for ( let i = 0; i < 5; i++) {
+    const elementIndex = (i+1)
+    document.getElementById("img" +elementIndex).src=data.hits[i].recipe.image 
+    document.getElementById("link" +elementIndex).href=data.hits[i].recipe.url 
+    document.getElementById("link" +elementIndex).innerHTML=data.hits[i].recipe.label
+}
 
-document.getElementById("link1").href=data.hits[0].recipe.url 
-document.getElementById("link2").href=data.hits[1].recipe.url
-document.getElementById("link3").href=data.hits[2].recipe.url
+// document.getElementById("img1").src=data.hits[0].recipe.image 
+// document.getElementById("img2").src=data.hits[1].recipe.image
+// document.getElementById("img3").src=data.hits[2].recipe.image
 
-document.getElementById("link1").innerHTML=data.hits[0].recipe.label
-document.getElementById("link2").innerHTML=data.hits[1].recipe.label
-document.getElementById("link3").innerHTML=data.hits[2].recipe.label
+// document.getElementById("link1").href=data.hits[0].recipe.url 
+// document.getElementById("link2").href=data.hits[1].recipe.url
+// document.getElementById("link3").href=data.hits[2].recipe.url
 
-document.getElementById("carouselExampleControls").classList.remove("hidden")
-// class="d-block w-100" alt="...">
-//         <h5 class="carousel-title">${data.hits[0].recipe.label}</h5>
-//         <a href="${data.hits[0].recipe.url}" class="btn btn-primary">Select</a>
-//       </div>
-//       <div class="carousel-item">
-//         <img src="${data.hits[1].recipe.image}" class="d-block w-100" alt="...">
-//         <h5 class="carousel-title">${data.hits[1].recipe.label}</h5>
-//         <a href="${data.hits[1].recipe.url}" class="btn btn.primary">Select</a>
-//       </div>
-//     </div>
-//     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-//       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-//       <span class="visually-hidden">Previous</span>
-//     </button>
-//     <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-//       <span class="carousel-control-next-icon" aria-hidden="true"></span>
-//       <span class="visually-hidden">Next</span>
-//     </button>
-//     </div>
-// `
+// document.getElementById("link1").innerHTML=data.hits[0].recipe.label
+// document.getElementById("link2").innerHTML=data.hits[1].recipe.label
+// document.getElementById("link3").innerHTML=data.hits[2].recipe.label
+
+document.getElementById("carousel").classList.remove("hidden")
 } 
+
+function populateCarousel(){
+    for ( let i = 0; i < 5; i++) {
+        const elementIndex = (i+1);
+        let carousel = document.createElement("div");
+        carousel.classList.add("carousel-item");
+        if (i === 0) {
+            carousel.classList.add("active")
+        }
+        let image = document.createElement("img");
+        image.classList.add("d-block");
+        image.setAttribute('id', "img" +elementIndex);
+        let link = document.createElement("a");
+        link.setAttribute('id', "link" +elementIndex);
+        link.setAttribute('target', "_blank");
+        carousel.appendChild(image);
+        carousel.appendChild(link);
+        carouselInner = document.querySelector(".carousel-inner")
+        carouselInner.appendChild(carousel);
+    }
+}
+
+// function useApiData(data) {
+//     document.querySelector("a href=")
+//     link.addEventListener("click", function(groceryList){
+//         array = [ingredientLines]
+//     })
+// }
+
+
+
+
+
+
+
+
 
 
 
